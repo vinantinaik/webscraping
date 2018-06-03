d70c6114fe241b37f41a4b452495118303d73146
Vinanti <vinantinaik@gmail.com>
2018-06-03 18:10:43 -0500

    corrected port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var db = require("./models");
 
 
 //Port
-var PORT =  process.env.Port || 3000;
+var PORT =  process.env.PORT || 3000;
 
 //Initialize Express
 var app = express();
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI ="mongodb://<dbuser>:<dbpassword>@ds247170.mlab.com:47170/heroku_p4rnx90h" || "mongodb://localhost/webscrapper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscrapper";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
